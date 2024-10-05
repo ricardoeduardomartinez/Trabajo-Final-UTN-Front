13c6a4028f7f32efedb9d726d757c124732c82d6
Ricardo Martinez <134613783+ricardoeduardomartinez@users.noreply.github.com>
2024-10-05 10:47:36 -0300

    casa 3

---
+++ src/encabezaCuotasYEnvios.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EncabezaCuotasYEnvios = () => {
+  return (
+    <>
+      <h3>¡6 cuotas sin interés! ¡Envíos a todo el país!</h3>
+    </>
+  );
+}
+
+export default EncabezaCuotasYEnvios;
